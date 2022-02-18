adca5e604a4b4e958ed6b41970dae4d5955f1ecd
AAKASH VERMA <98387181+Aakash0012@users.noreply.github.com>
2022-02-18 09:13:37 +0530

    Add files via upload

---
+++ github work/app.py
@@ -0,0 +1,21 @@
+from xml.dom.pulldom import ErrorHandler
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    h=[]
+    for i in range(1, 11):
+        h.append(i*3)
+    return render_template('index.html', h=h)
+
+@app.route('/greet')
+def greet():
+     return render_template('greet.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return ( render_template('404.html'), 404)
+
+if __name__== '__main__':
+    app.run(debug=True)
